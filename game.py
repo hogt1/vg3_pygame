336789ee40b9952ab988355fae572514e96ae328
Tom Øyvind Hogstad <hogt1@skole.t-fk.no>
2019-03-28 11:19:48 +0100

    Skaler bilde

--- game.py
+++ game.py
@@ -14,9 +14,15 @@ surface = pygame.Surface(screen.get_size())
 surface.convert()
 
 filename = os.path.join('resources', 'ballon_1.png')
-print(filename)
+
+
+object_rect = pygame.Rect(0,0, 100, 100)
+
 ballon_1 = pygame.image.load(filename)
 ballon_1.convert()
+
+ballon_1_fit_rect = ballon_1.get_rect().fit(object_rect)
+print(ballon_1_fit_rect.size)
 
 while True:
     pygame.event.pump()
@@ -26,7 +32,6 @@ while True:
             sys.exit()
     surface.fill((255, 255, 255))
     screen.blit(surface, (0,0))
-    screen.blit(ballon_1, (0,0))
-
+    screen.blit(pygame.transform.smoothscale(ballon_1, ballon_1_fit_rect.size), (100,100))
     pygame.display.flip()
     pygame.display.update()
