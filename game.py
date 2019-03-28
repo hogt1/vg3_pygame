b12573c3505787afe0d0913c829a73f31f8408a7
Tom Øyvind Hogstad <hogt1@skole.t-fk.no>
2019-03-28 10:11:41 +0100

    fill og blit

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from pygame.locals import *
 
@@ -9,4 +10,17 @@ SCREEN_HEIGHT = 720
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 surface = pygame.Surface(screen.get_size())
+surface.convert()
 
+
+while True:
+    pygame.event.pump()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    surface.fill((255, 255, 255))
+    screen.blit(surface, (0,0))
+
+    #pygame.display.flip()
+    pygame.display.update()
